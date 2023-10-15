66290b6bc291e1e4e599043c8b870b4460f1f8c3
stark5200 <a.taha5200@gmail.com>
2023-10-15 16:09:09 +0200

    added custom line svg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,20 @@
+import stark from "../../public/images/stark.jpg"
+import { motion } from "framer-motion"
+import { useState } from "react"
+
 export default function Nav(){
+  const [toggled, setToggled] = useState(false)
   return(
-    <nav>
-      Home
+    <nav className="relative mx-8 mb-24">
+      <svg 
+        width="250"
+        height={4}
+        viewBox="0 0 250 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M2 2L428 2" stroke="#282828" strokeWidth={2} strokeLinecap="round"/>
+      </svg>
     </nav>
   )
 }  
