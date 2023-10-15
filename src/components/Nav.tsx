2a9cc98de140d6b9beb3cb8bf0713a983eae1b14
stark5200 <a.taha5200@gmail.com>
2023-10-16 01:09:37 +0200

    Nav done, Halfway through

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,24 @@ import { motion } from "framer-motion"
 import { useState } from "react"
 import { useMediaQuery } from "../utils/useMediaQuery"
 
+const navMotion = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren", 
+      staggerChildren: 0.15,
+    },
+  },
+  hidden: {
+    opacity: 0, 
+  },
+}
+
+const itemMotion = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x:-100 },
+}
+
 export default function Nav(){
   const [toggled, setToggled] = useState(false)
   const matches = useMediaQuery('(min-width: 1280px)')
@@ -30,16 +48,36 @@ export default function Nav(){
         <a href="/">Stark.</a>
       </h1>
 
+      {/* Check if we are on mobile or not */}
+      {matches && (
+        <div className="flex gap-12">
+          <motion.a variants={itemMotion} href="/">Home</motion.a>
+          <motion.a variants={itemMotion} href="/services">Services</motion.a>
+          <motion.a variants={itemMotion} href="/contact">Contact</motion.a>
+        </div>
+      )}
+
       {!matches && (
         <div 
           onClick={() => setToggled((prevToggle) => !prevToggle)} 
-          className="space-y-1 cursor-pointer"
+          className="space-y-1.5 cursor-pointer z-10"
         >
-          <span className="block h-0.5 w-8 bg-black"></span>
-          <span className="block h-0.5 w-5 bg-black"></span>
-          <span className="block h-0.5 w-5 bg-black"></span>
+          <motion.span animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }} className="block h-0.5 w-8 bg-black"></motion.span>
+          <motion.span animate={{ width: toggled ? 0 : 24}} className="block h-0.5 w-5 bg-black"></motion.span>
+          <motion.span animate={{ rotateZ: toggled ? -45 : 0, width: toggled ? 32 : 16, y: toggled ? -8 : 0}} className="block h-0.5 w-5 bg-black"></motion.span>
         </div>
       )}
+
+      {toggled && !matches && (
+        <motion.div animate={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 25 }} className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center">
+          <motion.div variants={navMotion} animate="visible" initial="hidden" className="flex flex-col gap-24 text-lg">
+          <motion.a variants={itemMotion} href="/">Home</motion.a>
+          <motion.a variants={itemMotion} href="/services">Services</motion.a>
+          <motion.a variants={itemMotion} href="/contact">Contact</motion.a>
+          </motion.div>
+        </motion.div>
+      )}
+
     </nav>
   )
 }  
